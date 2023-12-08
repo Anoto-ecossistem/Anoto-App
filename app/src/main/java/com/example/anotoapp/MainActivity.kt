fa3cb381adcc9fbaca905426f9f9a960356ed922
Matheus Alves <matheusalvessilvabr@gmail.com>
2023-12-08 17:59:56 -0300

    add navigation bottom for home page task page and profile page

--- app/src/main/java/com/example/anotoapp/MainActivity.kt
+++ app/src/main/java/com/example/anotoapp/MainActivity.kt
@@ -1,15 +1,40 @@
 package com.example.anotoapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.fragment.app.Fragment
+import com.example.anotoapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         installSplashScreen()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        loadFragment(HomeFragment())
+
+        binding.bottomNav.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.home -> loadFragment(HomeFragment())
+                R.id.task -> loadFragment(TaskFragment())
+                R.id.profile -> loadFragment(ProfileFragment())
+
+                else ->{
+                }
+            }
+            true
+        }
     }
 
-        
+
+
+    private fun loadFragment(fragment: Fragment) {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.frameLayout, fragment)
+        transaction.commit()
+    }
 }
